2a7642b36ed1418a3a4940629898404d6381a597
danielvelazquezl <daniel.velazquez@fiuni.edu.py>
2019-03-29 11:17:00 -0400

    adding new files

--- matriz.c
+++ matriz.c
@@ -6,10 +6,10 @@ int orden; //tamanho de la matriz
 
 int** reservarMemoria();
 void cargarMatriz(int **matriz, char* nombre);
+void leerMatriz(int **matriz, char* nombre);
 void imprimirMatriz(int **matriz);
 void liberarMemoria(int **matriz);
 void multiplicar(int **a, int **b, int **res);
-
 
 
 /*********************************************************************************/
@@ -24,44 +24,40 @@ int main(){
 	srand(time(NULL));
 	printf("Ingrese el orden de la matriz: ");
 	scanf("%d",&orden);
+	
 	matrizA = reservarMemoria();
 	matrizB = reservarMemoria();
-
 	cargarMatriz(matrizA, "mat_A.txt");
+	cargarMatriz(matrizB, "mat_B.txt");
+	liberarMemoria(matrizA);
+	liberarMemoria(matrizB);
 
-	FILE *infile; 
-    // Open matriz_name.txt for reading 
-    infile = fopen ("mat_A.txt", "r"); 
-    if (infile == NULL) 
-    { 
-        fprintf(stderr, "\nError opening file\n"); 
-        exit (1); 
-    } 
-      
-    // read file contents till end of file
-    fread(matrizB[0], sizeof(int), 2, infile);
-    fread(matrizB[1], sizeof(int), 2, infile); 
-    //fread(&matrizB[0][1], sizeof(int), 1, infile);
-   imprimirMatriz(matrizB);
-  
-    // close file 
-    fclose (infile); 
+	matrizA = reservarMemoria();
+	matrizB = reservarMemoria();
+	leerMatriz(matrizA, "mat_A.txt");
+	leerMatriz(matrizB, "mat_B.txt");
 
-	//cargarMatriz(matrizB);
-
-	/*printf("Matriz A\n");
 	imprimirMatriz(matrizA);
-	printf("Matriz B\n");
-	imprimirMatriz(matrizB);*/
+    imprimirMatriz(matrizB);
 
-	//multiplicar(matrizA, matrizB, resultado);
-
-	//printf("Matriz resultante\n");
-	//imprimirMatriz(resultado);
+    resultado = reservarMemoria();
+	multiplicar(matrizA, matrizB, resultado);
+	leerMatriz(resultado, "resultado.txt");
+	imprimirMatriz(resultado);
 
 	liberarMemoria(matrizA);
-	//liberarMemoria(matrizB);
-	//liberarMemoria(resultado);
+	liberarMemoria(matrizB);
+	liberarMemoria(resultado);
+
+	short i= 5;
+	short c= 95;
+	FILE *outfile;
+	outfile = fopen("mat_A.txt", "r");
+	//fwrite(&i, sizeof(short), 1, outfile);
+	//fwrite(&c, sizeof(short), 1, outfile);
+	fseek(outfile, 0L, SEEK_END);
+  	printf("test.c ocupa %d bytes", ftell(outfile)/4);
+	fclose(outfile);
 
 	system("PAUSE");
 	return 0;
@@ -92,8 +88,7 @@ void cargarMatriz(int **matriz, char* nombre){
 			
 			printf("%d ", matriz[i][j]);
 		}
-		fwrite(matriz[i], sizeof(int), 2, outfile);
-		
+		fwrite(matriz[i], sizeof(int), orden, outfile);	
 	}
 	
 	if(fwrite != 0)  
@@ -101,6 +96,23 @@ void cargarMatriz(int **matriz, char* nombre){
     else 
         printf("error writing file !\n");
 	fclose(outfile);
+}
+
+void leerMatriz(int **matriz, char* nombre){
+	FILE *infile; 
+    // Open matriz_name.txt for reading 
+    infile = fopen (nombre, "r"); 
+    if (infile == NULL) 
+    { 
+        fprintf(stderr, "\nError opening file\n"); 
+        exit (1); 
+    } 
+      
+    // read file contents till end of file
+    for(int i=0; i<orden; i++){
+    	fread(matriz[i], sizeof(int), orden, infile);
+    }
+    fclose (infile); 
 }
 
 void imprimirMatriz(int **matriz){
@@ -124,6 +136,13 @@ void liberarMemoria(int **matriz){
 
 void multiplicar(int **a, int **b, int **res){
 	int i,j,k;
+	FILE *outfile;
+	outfile = fopen("resultado.txt", "w");
+	if (outfile == NULL) 
+    { 
+        fprintf(stderr, "\nError opend file\n"); 
+        exit (1); 
+    } 
 	for (i=0;i<orden;i++){
 		for (j=0;j<orden;j++){ 
 			res[i][j]=0;
@@ -131,5 +150,11 @@ void multiplicar(int **a, int **b, int **res){
 				res[i][j]=res[i][j]+a[i][k]*b[k][j];
 			}
 		}
+		fwrite(res[i], sizeof(int), orden, outfile);	
     }
+    if(fwrite != 0)  
+        printf("contents to file written successfully !\n"); 
+    else 
+        printf("error writing file !\n");
+	fclose(outfile);
 }
